de032350092abc2bc67680c00e0be9b8b4d15b2c
laudbrian <laudbrian@gmail.com>
2014-08-22 21:38:32 -0700

    add models and collections

--- js/app/app.js
+++ js/app/app.js
@@ -1,16 +1,33 @@
 define([
     'app/views/app',
-    'app/routers/router'
-],  function (AppView, Router) {
+    'app/routers/router',
+    'app/models/app',
+    'app/collections/days'
+],  function (AppView, Router, AppModel, DaysCollection) {
 
     'use strict';
 
     var initialize = function () {
-        var appView = new AppView();
-        $('body').append(appView.el);
+        var appModel = new AppModel();
+
+        var appView = new AppView({model: appModel});
+        $('body').append(appView.render().el);
 
         var router = new Router(appView);
         Backbone.history.start();
+
+        var daysCollection = new DaysCollection([], {
+            url: 'http://api.wunderground.com/api/731d77254a0f25fb/forecast/q/IT/Magenta.json'
+        });
+
+        daysCollection.fetch({
+            success: function (collection, response, options) {
+                console.log(collection, response);
+            },
+            error: function (collection, response, options) {
+                console.log('error');
+            }
+        });
     };
 
     return {
